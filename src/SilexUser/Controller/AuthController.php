b9267ab5334a9706cf1721e95f724423b86634dc
Ben <benedict@phpdevel.net>
2013-11-01 14:42:32 +0100

    Persist user

--- src/SilexUser/Controller/AuthController.php
+++ src/SilexUser/Controller/AuthController.php
@@ -4,6 +4,8 @@ namespace SilexUser\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use SilexUser\Form\UserType;
 use SilexUser\User;
 
@@ -19,14 +21,25 @@ class AuthController
 
     public function register(Request $request, Application $app)
     {
-        $form = $app['form.factory']->create($app['silex_user.form.registration'], new User());
+        $form = $app['form.factory']->create($app['silex_user.form.registration']);
+        $em = $app['silex_user.entity_manager'];
 
         $form->handleRequest($request);
         if ($form->isValid()) {
+            $em->getConnection()->beginTransaction();
             try {
+                $user = $form->getData();
+                $em->persist($user);
+                $em->flush();
+                $em->getConnection()->commit();
+
+                return $app->redirect('/login');
 
             } catch (\Exception $e) {
+                $em->getConnection()->rollback();
+                $em->close();
 
+                $form->addError(new FormError(sprintf('Registration failed %s', $app['debug'] ? $e->getMessage() : null)));
             }
         }
 
